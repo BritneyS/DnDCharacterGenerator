45a9dd0053f349932eef61d2f614d0c0d97f0435
Britney Smith <brit.smith7@gmail.com>
2019-05-13 15:36:15 -0400

    Fetch is retried on generateButton tap if it failed the first time; some refactors

--- DnDCharacterGenerator/ViewController.swift
+++ DnDCharacterGenerator/ViewController.swift
@@ -31,22 +31,13 @@ class ViewController: UIViewController {
     
     var gradientLayer: CAGradientLayer!
     let characterClassQuery = CharacterClassQuery()
+    var isDataFetched: Bool = false
     var allCharacterClasses: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        classLabel.text = "\t"
-        raceLabel.text = "\t"
-        alignmentLabel.text = "\t"
-        ApolloService.shared.client.fetch(query: characterClassQuery) { results, error in
-            if error != nil {
-                print("⛔️ Error in fetching response: \(String(describing: error))")
-            } else if let results = results?.data?.classResult?.allClasses?.compactMap({ $0 }) {
-                print(results)
-                self.allCharacterClasses = results.map{ $0.name! }
-                print("⚔️ Classes: \(self.allCharacterClasses)")
-            }
-        }
+        clearLabels()
+        tryDataFetch()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -54,7 +45,37 @@ class ViewController: UIViewController {
         createGradientLayer()
     }
     
-    func createGradientLayer() {
+    private func clearLabels() {
+        classLabel.text = "\t"
+        raceLabel.text = "\t"
+        alignmentLabel.text = "\t"
+    }
+    
+    private func populateLabels() {
+        if isDataFetched == false {
+            clearLabels()
+        } else {
+            classLabel.text = allCharacterClasses.randomElement()
+            raceLabel.text = "Half-Orc"
+            alignmentLabel.text = "Chaotic Neutral"
+        }
+    }
+    
+    private func tryDataFetch() {
+        ApolloService.shared.client.fetch(query: characterClassQuery) { results, error in
+            if error != nil {
+                self.isDataFetched = false
+                print("⛔️ Error in fetching response: \(String(describing: error))")
+            } else if let results = results?.data?.classResult?.allClasses?.compactMap({ $0 }) {
+                self.isDataFetched = true
+                print(results)
+                self.allCharacterClasses = results.map{ $0.name! }
+                print("⚔️ Classes: \(self.allCharacterClasses)")
+            }
+        }
+    }
+    
+    private func createGradientLayer() {
         gradientLayer = CAGradientLayer()
         gradientLayer.frame = backgroundImageView.bounds
         gradientLayer.colors = [UIColor.clear.cgColor, UIColor(named: "LightColor")!.cgColor]
@@ -64,9 +85,12 @@ class ViewController: UIViewController {
     }
 
     @IBAction func generateButtonTapped(_ sender: UIButton) {
-        classLabel.text = allCharacterClasses.randomElement()
-        raceLabel.text = "Half-Orc"
-        alignmentLabel.text = "Chaotic Neutral"
+        if isDataFetched == false {
+            tryDataFetch()
+            populateLabels()
+        } else {
+            populateLabels()
+        }
     }
     
     
